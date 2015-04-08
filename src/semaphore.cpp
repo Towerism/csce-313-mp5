2d3f460e08091fd75f4a76bf567fcfb120be5d6f
Martin Fracker <martin.frackerjr@gmail.com>
2015-04-08 02:49:39 -0500

    Implemented semaphore

--- src/semaphore.cpp
+++ src/semaphore.cpp
@@ -1,19 +1,33 @@
 #include "semaphore.h"
 
-Semaphore::Semaphore(int _val) {
-
+Semaphore::Semaphore(int _val) : value(_val) {
+    pthread_mutex_init(&m, NULL);
+    pthread_cond_init(&c, NULL);
 }
 
 Semaphore::~Semaphore() {
-
+    pthread_mutex_destroy(&m);
+    pthread_cond_destroy(&c);
 }
 
-// figure out what this shit does, and rename it
 int Semaphore::P() {
+    pthread_mutex_lock(&m);
+
+    while(value == 0) {
+        pthread_cond_wait(&c, &m);
+    }
+    value -= 1;
+
+    pthread_mutex_unlock(&m);
     return 0;
 }
 
-// "
 int Semaphore::V() {
+    pthread_mutex_lock(&m);
+
+    value += 1;
+    pthread_cond_signal(&c);
+
+    pthread_mutex_unlock(&m);
     return 0;
 }
