1aeee4bfa6348b7a19a773419f66f8513125d78b
Martin Fracker <martin.frackerjr@gmail.com>
2015-04-13 19:05:46 -0500

    Styling issue

--- test/semaphoreThreadTest.cpp
+++ test/semaphoreThreadTest.cpp
@@ -5,7 +5,7 @@
 TEST_F(SemaphoreThreadTest, SingleThread) {
     threads.push_back(std::thread(Runnable::run_thread, task));
     threads[0].join();
-    
+
     ASSERT_EQ(100, task->get_cash());
 }
 
@@ -13,9 +13,6 @@ TEST_F(SemaphoreThreadTest, MultiThread) {
     for (int i = 0; i < 32; ++i) {
         threads.push_back(std::thread(Runnable::run_thread, task));
     }
-    for (auto& t : threads) {
-        t.join();
-    }
+    for (auto& t : threads) { t.join(); }
     ASSERT_EQ(100, task->get_cash());
-
 }
