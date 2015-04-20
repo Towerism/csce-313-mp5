92af2f2a5d128a192cfef4ba83110d057e00b02b
Martin Fracker <martin.frackerjr@gmail.com>
2015-04-20 00:10:06 -0500

    Got working without graphics

--- src/worker_task.cpp
+++ src/worker_task.cpp
@@ -1,7 +1,9 @@
 #include <string>
+#include <cstring>
 #include <unistd.h>
 #include "worker_task.h"
 #include "bounded_buffer.h"
+#include "util.h"
 
 Worker_task::Worker_task(Bounded_buffer<Data>* buf, Buffer_filter& out_bufs, RequestChannel& ctrl) : buffer(buf), out_buffers(out_bufs), channel(channel) {
   std::string chan_handle = ctrl.send_request("newthread");
@@ -13,6 +15,9 @@ void Worker_task::run() {
     while (!cancelled) {
         Data d = buffer->dequeue();
         std::string response = channel->send_request(d.data);
-        out_buffers.push(Data(d.request_origin, response));
+        // make sure the response is a number before putting it in out_buffers
+        if (is_number(response)) {
+          out_buffers.push(Data(d.request_origin, response));
+        }
     }
 }
