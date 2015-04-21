e219a1b5e7dc9c6d0ee3083e2073dc7b3028c19a
Martin Fracker <martin.frackerjr@gmail.com>
2015-04-20 21:37:59 -0500

    Tweak tests to perform more worker thread count increases

--- performance_tester.py
+++ performance_tester.py
@@ -6,11 +6,10 @@ from functools import wraps
 def test_performance():
     testing_buf = [10, 100, 1000, 10000]
     #testing_work = [10, 100, 1000, 10000]
-    testing_work = [5, 10, 25, 10000]
-    number_of_tests = 4
-    for i in range(number_of_tests):
+    testing_work = [5, 10, 25, 50, 100, 150]
+    for i in range(len(testing_buf)):
         print "---------- buffer size = " + str(testing_buf[i]) + " ----------"
-        for j in range(number_of_tests):
+        for j in range(len(testing_work)):
             print "worker thread count = " + str(testing_work[j])
             #-n = request count | -b is buffer size | -w is worker thread count | -x specifies no graphics
             start = time.time()
