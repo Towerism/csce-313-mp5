368c4572f3577bfcac052813080ac7da966bb8e5
Christopher Findeisen <cfindeisen7@gmail.com>
2015-04-29 13:29:29 -0500

    Modified constructors

--- include/network_req_channel.h
+++ include/network_req_channel.h
@@ -7,6 +7,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include "client_socket.h"
 
 class NetworkRequestChannel {
  public:
@@ -14,23 +15,31 @@ class NetworkRequestChannel {
  private:
   int my_sockfd; //should be already connected
   std::string my_name;
+  std::string hostname;
+  int port;
   Side my_side;
 
  public:
-  NetworkRequestChannel(const std::string _name, const Side _side, int _sockfd);
+  NetworkRequestChannel( std::string _name,  Side _side, std::string _hostname, int _port);
+  NetworkRequestChannel( std::string _name,  Side _side, int _sockfd);
   ~NetworkRequestChannel();
 
   /* Send a string over the channel and wait for a reply. */
   std::string send_request(std::string _request);
 
   std::string cread();
+  std::string cread(int sockfd);
   /* Blocking read of data from the channel. Returns a string of characters
      read from the channel. Returns NULL if read failed. */
 
   int cwrite(std::string _msg);
+  int cwrite(std::string _msg, int sockfd);
   /* Write the data to the channel. The function returns the number of characters written
      to the channel. */
 
   inline std::string get_name(){return my_name;}
+  inline std::string get_hostname(){return hostname;}
   inline int get_sockfd(){return my_sockfd;}
+  inline Side get_side(){return my_side;}
+  inline int get_port(){return port;}
 };
--- src/worker_task.cpp
+++ src/worker_task.cpp
@@ -5,10 +5,12 @@
 #include "bounded_buffer.h"
 #include "util.h"
 
-Worker_task::Worker_task(Bounded_buffer<Data>* buf, Buffer_filter& out_bufs, NetworkRequestChannel& ctrl) : buffer(buf), out_buffers(out_bufs), channel(channel) {
+Worker_task::Worker_task(Bounded_buffer<Data>* buf, Buffer_filter& out_bufs, NetworkRequestChannel& ctrl) : buffer(buf), out_buffers(out_bufs) {
+  std::cout << "" << std::endl;
   std::string chan_handle = ctrl.send_request("newthread");
   //std::cout << "chan_handle: " << chan_handle << std::endl;
-  channel = new NetworkRequestChannel(chan_handle, NetworkRequestChannel::CLIENT_SIDE, 0);
+  //int client_sockfd = connect_to_server(server_hostname, port);
+  channel = new NetworkRequestChannel(chan_handle, NetworkRequestChannel::CLIENT_SIDE, ctrl.get_hostname(), ctrl.get_port());
 }
 
 void Worker_task::run() {
