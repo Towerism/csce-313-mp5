bdc445da6dfbcb09a75825e6c18790a18048032b
Martin Fracker <martin.frackerjr@gmail.com>
2015-05-03 19:06:19 -0500

    Kill data server processes, fix array in loop

--- testing.sh
+++ testing.sh
@@ -1,18 +1,12 @@
 #! /bin/bash
 
 REQUESTS=1000
-PORT=97450
+PORT=99450
 BACKLOG=64
+DSERVPID=
 CLIENTS=
 
-declare -a CLIENT_ARRAY
-CLIENT_ARRAY[0]=1
-CLIENT_ARRAY[1]=2
-CLIENT_ARRAY[2]=4
-CLIENT_ARRAY[3]=8
-CLIENT_ARRAY[4]=16
-CLIENT_ARRAY[5]=32
-CLIENT_ARRAY[6]=64
+CLIENT_ARRAY=(1 2 4 8 16 32 64)
 echo "client array is ${CLIENT_ARRAY[*]}"
 
 # empty the logging file
@@ -27,49 +21,53 @@ echo "==================================" >> timing.log
 
 # server with backlog 64
 ./data_server -p $PORT -b $BACKLOG &
+DATASERVPID=$!
 
-for i in $CLIENT_ARRAY; do
-    echo $CLIENT_ARRAY
+for i in ${CLIENT_ARRAY[*]}; do
     CLIENTS=$i
-    echo $CLIENTS
     exectest
 done
+
+kill -KILL $DATASERVPID
 
 
 # server with backlog 128
 let "PORT++"
 BACKLOG=128
 ./data_server -p $PORT -b $BACKLOG &
+DATASERVPID=$!
 
-for i in $CLIENT_ARRAY; do
-    echo $CLIENT_ARRAY
+for i in ${CLIENT_ARRAY[*]}; do
     CLIENTS=$i
-    echo $CLIENTS
     exectest
 done
+
+kill -KILL $DATASERVPID
 
 
 # server with backlog 256
 let "PORT++"
 BACKLOG=256
 ./data_server -p $PORT -b $BACKLOG &
+DATASERVPID=$!
 
-for i in $CLIENT_ARRAY; do
-    echo $CLIENT_ARRAY
+for i in ${CLIENT_ARRAY[*]}; do
     CLIENTS=$i
-    echo $CLIENTS
     exectest
 done
+
+kill -KILL $DATASERVPID
 
 
 # server with backlog 512
 let "PORT++"
 BACKLOG=512
 ./data_server -p $PORT -b $BACKLOG &
+DATASERVPID=$!
 
-for i in $CLIENT_ARRAY; do
-    echo $CLIENT_ARRAY
+for i in ${CLIENT_ARRAY[*]}; do
     CLIENTS=$i
-    echo $CLIENTS
     exectest
 done
+
+kill -KILL $DATASERVPID
